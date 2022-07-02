86bd2112712ec7afa547bb2f8614790ace6856ad
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2022-07-02 13:21:18 +0000

    Setup PWA

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import "../styles/global.css";
 import "../styles/carbon.css";
 import Head from "next/head";
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   const title = "Can I DevTools?"
   const description = "It is like @CanIUse, but for the browser devtools, created and curated by Pankaj Parashar."
@@ -13,7 +12,7 @@ export default function MyApp({ Component, pageProps }) {
         <title>{title}</title>
 
         <meta name="title" content={title} />
-        <meta name="viewport" content="width=device-width, initial-scale=0.9" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.9, viewport-fit=cover" />
         <meta
           name="description"
           content={description}
@@ -29,6 +28,18 @@ export default function MyApp({ Component, pageProps }) {
         />
         <meta name="twitter:site" content="@CanIDevTools" />
         <meta name="twitter:creator" content="@pankajparashar" />
+        
+        <meta name='application-name' content={title} />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+        <meta name='apple-mobile-web-app-title' content={title} />
+        <meta name='description' content={description} />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
+        <meta name='msapplication-config' content='/browserconfig.xml' />
+        <meta name='msapplication-TileColor' content='#2B5797' />
+        <meta name='msapplication-tap-highlight' content='no' />
+        <meta name='theme-color' content='#000000' />
       </Head>
       <Component {...pageProps} />
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -40,6 +40,7 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/favicon-16x16.png"
           />
+          <link rel='manifest' href='/manifest.json' />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
           <meta
