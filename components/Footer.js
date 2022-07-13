2a64a63409fb97c402d7ce1efdbd3f2919450b2c
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2022-07-13 06:08:29 +0000

    Updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,44 +8,37 @@ export function Footer() {
         <summary className={`bgc_000 ai_center p_1em`}>About</summary>
         <div className={`d_grid gtc_320px`}>
           <div className="p_1em bb_1px_dark br_1px_dark pb_0 pt_0">
-            <p className="has-dropcap">
+            <p style={{ textIndent: "3em" }}>
                 <strong>Can I DevTools?</strong> is like{" "}
                 <a href="https://twitter.com/caniuse">@CanIUse</a> but for the
-                browser devtools. It is created by{" "}
+                browsers devtools. It is created by{" "}
                 <a href="https://twitter.com/pankajparashar">Pankaj Parashar</a>{" "}
-                and curated by the community. There is a lot of coverage on browser devtools just exclusively
+                and curated by the community.</p> 
+            <p style={{ textIndent: "3em" }}>
+              There is a lot of coverage on browser devtools just exclusively
                 focused on Chrome. This attempts to fix that by comparing and contrasting devtool features across
                 all major browsers.
               </p>
-            <dl>
-              <dt>
-                <strong>Github</strong> / <strong>Twitter:</strong>
-              </dt>
-              <dd className={`mb_1em`}>
-                <a href="https://github.com/pankajparashar/canidev.tools">
-                  canidev.tools
-                </a>
-                {" / "}
-                <a href="https://twitter.com/CanIDevTools">@CanIDevTools</a>
-              </dd>
-            </dl>
+              <div className="d_grid gap_5px ta_center" style={{ gridTemplateColumns: "repeat(3, 1fr)" }}>
+                <div className="bgc_000 p_05em"><a href="https://github.com/pankajparashar/canidev.tools">Github</a></div>
+                <div className="bgc_000 p_05em"><a href="https://twitter.com/CanIDevTools">Twitter</a></div>
+                <div className="bgc_000 p_05em"><a href="https://canidevtools.substack.com/feed">RSS</a></div>
+              </div>
           </div>
           <div className="p_1em bb_1px_dark br_1px_dark">
 
             <div className="wrapper"><iframe src="https://canidevtools.substack.com/embed" width="480" height="320" frameBorder="0" scrolling="no"></iframe></div>
 
-            <div className="mt_1em">
+          </div>
+          <div className="p_1em bb_1px_dark d_flex jc_center ai_center fd_col gap_1em">
+            <CarbonAds />
+            <div className="d_grid gap_5px" style={{ gridTemplateColumns: "repeat(2, 1fr)" }}>
               <button
-                style={{ width: "auto" }}
-                className="bb_1px_dashed"
                 onClick={() => { }}
               >
                 Day/Night
               </button>
-              {" | "}
               <button
-                style={{ width: "auto" }}
-                className="bb_1px_dashed"
                 onClick={() =>
                   document.body.scrollIntoView({ behavior: "smooth" })
                 }
@@ -53,9 +46,6 @@ export function Footer() {
                 Top ↑
               </button>
             </div>
-          </div>
-          <div className="p_1em bb_1px_dark d_flex jc_center ai_center">
-            <CarbonAds />
           </div>
         </div>
       </details>
