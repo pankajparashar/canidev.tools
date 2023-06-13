a1c2046588164037b8621575ce20a13f9abadcd7
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-06-13 08:44:15 +0530

    Fixed react class names

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -287,9 +287,9 @@ export default function Index() {
 }
 
 const IconBrandPolypane = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-polypane" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-polypane" width="20" height="20" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
         <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
         <path d="M 21,12 H 15 M 15,4 V 14 H 9 M 9,4 V 16 H 4" />
     </svg>
-);
+  );
