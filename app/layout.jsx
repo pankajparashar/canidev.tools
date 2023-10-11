999e132e30238415d1a28920029e46316b64aadd
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-10-11 08:07:19 +0100

    switched to api

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,8 +2,6 @@ import { Analytics } from "@vercel/analytics/react";
 
 import "@mantine/core/styles.css";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
-import * as fs from "fs";
-import path from "path";
 
 import "../public/style.css";
 
@@ -16,15 +14,9 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
+    const features = await fetch(`https://canidev.tools/data.json`).then(res => res.json());
     const categories = {};
-    const features = [];
-
-    fs.readdirSync("features").forEach(name => {
-        const filename = path.join("features", name);
-        const file = fs.readFileSync(filename);
-        const feature = JSON.parse(file);
-        features.push(feature);
-
+    features.forEach(feature => {
         const category = feature.Category;
         categories[category] = category in categories ? categories[category] + 1 : 1;
     });
