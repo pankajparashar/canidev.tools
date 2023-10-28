65e742b822e3b31e92054eed0d72d3b488571009
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-10-28 21:32:57 +0100

    added app icons

--- app/layout.jsx
+++ app/layout.jsx
@@ -47,6 +47,19 @@ export default async function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link
+                  rel="icon"
+                  href="/icon?<generated>"
+                  type="image/<generated>"
+                  sizes="<generated>"
+                />
+                <link
+                  rel="apple-touch-icon"
+                  href="/apple-icon?<generated>"
+                  type="image/<generated>"
+                  sizes="<generated>"
+                />
                 <ColorSchemeScript />
             </head>
             <body>
