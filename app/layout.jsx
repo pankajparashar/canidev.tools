83db7069f5c52a253888962d1943f1f31611d31e
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-11-04 16:46:46 +0000

    set maximum scalre

--- app/layout.jsx
+++ app/layout.jsx
@@ -48,6 +48,7 @@ export default async function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                <meta name="viewport" content="width=device-width, maximum-scale=1" />
                 <link rel="icon" href="/favicon.ico" sizes="any" />
                 <link
                   rel="apple-touch-icon"
