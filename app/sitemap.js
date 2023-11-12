af3e4ff6974d45713c4bb96870664a7b3461b682
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-11-13 01:48:55 +0530

    fixed path

--- app/sitemap.js
+++ app/sitemap.js
@@ -4,7 +4,7 @@ import path from "path";
 export default function sitemap() {
 	
 	const urlset = []
-	fs.readdirSync("features").forEach(filename => {
+	fs.readdirSync("../features").forEach(filename => {
 		const filepath = path.join("features", filename);
 		const fileobj = fs.readFileSync(filepath);
 		const mtime = fs.statSync(filepath).mtime
