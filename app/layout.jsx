17599430261db64c0a02f8d497612b8c0fadcfcd
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-11-16 16:53:51 +0530

    swicthed to sneak mono

--- app/layout.jsx
+++ app/layout.jsx
@@ -64,7 +64,7 @@ export default async function RootLayout({ children }) {
 	});
 
 	const theme = {
-		fontFamily: "ui-monospace",
+		fontFamily: "Sneak Mono",
 		fontFamilyMonospace: "ui-monospace",
 		primaryColor: "dark",
 		defaultRadius: 0,
