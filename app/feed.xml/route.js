623e1045aa333f4ede460f7b75fa5b21a0c6db67
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-12-21 20:29:49 +0000

    relative path

--- app/feed.xml/route.js
+++ app/feed.xml/route.js
@@ -5,36 +5,37 @@ import HJSON from "hjson";
 import RSS from "rss";
 
 export async function GET() {
-    const feed = new RSS({
-        title: "Can I DevTools?",
-        description: "It is like @CanIUse but for the browser devtools, created by @pankajparashar",
-        site_url: "https://canidev.tools",
-        feed_url: "https://canidev.tools/feed.xml",
-        copyright: "Pankaj Parashar",
-        language: "en-GB",
-        pubDate: new Date(),
-    });
+  const feed = new RSS({
+    title: "Can I DevTools?",
+    description:
+      "It is like @CanIUse but for the browser devtools, created by @pankajparashar",
+    site_url: "https://canidev.tools",
+    feed_url: "https://canidev.tools/feed.xml",
+    copyright: "Pankaj Parashar",
+    language: "en-GB",
+    pubDate: new Date(),
+  });
 
-    fs.readdirSync("features").forEach(filename => {
-        const filepath = path.join("features", filename);
-        const fileobj = fs.readFileSync(filepath, "utf-8");
-        const mtime = fs.statSync(filepath).mtime;
+  fs.readdirSync("features").forEach((filename) => {
+    const filepath = path.join("../features", filename);
+    const fileobj = fs.readFileSync(filepath, "utf-8");
+    const mtime = fs.statSync(filepath).mtime;
 
-        const feature = HJSON.parse(fileobj);
-        const slug = path.basename(filename, ".hjson");
-        feed.item({
-            title: feature.title,
-            url: `https://canidev.tools/${slug}`,
-            date: mtime,
-            description: feature.Description,
-            author: feature.Authors.join(", "),
-            categories: [feature.Category],
-        });
+    const feature = HJSON.parse(fileobj);
+    const slug = path.basename(filename, ".hjson");
+    feed.item({
+      title: feature.title,
+      url: `https://canidev.tools/${slug}`,
+      date: mtime,
+      description: feature.Description,
+      author: feature.Authors.join(", "),
+      categories: [feature.Category],
     });
+  });
 
-    return new Response(feed.xml({ indent: true }), {
-        headers: {
-            "Content-Type": "application/rss+xml; charset=utf-8",
-        },
-    });
+  return new Response(feed.xml({ indent: true }), {
+    headers: {
+      "Content-Type": "application/rss+xml; charset=utf-8",
+    },
+  });
 }
