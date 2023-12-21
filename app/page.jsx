e5dccdc74ad72e0a50ce25e07013423478e6a57c
Pankaj Parashar <parashar.p.pankaj@gmail.com>
2023-12-21 14:35:07 +0000

    updated category

--- app/page.jsx
+++ app/page.jsx
@@ -4,21 +4,10 @@ import { Paper } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
 
 export default function Page() {
-  const { height } = useViewportSize();
-  return (
-    <Paper
-      className="column d_flex ai_center"
-      w="100%"
-      withBorder
-      style={{ borderTop: 0, borderBottom: 0 }}
-    >
-      <iframe
-        height={Math.min(800, height - 50)}
-        id="testimonialto-f12-tag-all-light-animated"
-        src="https://embed-v2.testimonial.to/w/f12?animated=on&theme=dark&shadowColor=ffffff&speed=0.75&tag=all"
-        frameborder="0"
-        width="100%"
-      ></iframe>
-    </Paper>
-  );
+    const { height } = useViewportSize();
+    return (
+        <Paper className="column d_flex ai_center" w="100%" withBorder style={{ borderTop: 0, borderBottom: 0 }}>
+            <iframe height={Math.min(800, height - 50)} id="testimonialto-f12-tag-all-light-animated" src="https://embed-v2.testimonial.to/w/f12?animated=on&theme=dark&shadowColor=ffffff&speed=0.75&tag=all" frameborder="0" width="100%"></iframe>
+        </Paper>
+    );
 }
